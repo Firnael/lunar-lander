93f6afd02f30087f6b6b6df5444c4dfdc5aa6fd4
Audren <audren.burlot@gmail.com>
2023-04-10 23:16:12 +0200

    ✨ training - added ship and keybinds for training (needs work)

--- server/display/src/Game/Scenes/TrainingScene.ts
+++ server/display/src/Game/Scenes/TrainingScene.ts
@@ -1,6 +1,13 @@
 import 'phaser';
+import { LanderRotation } from '../../Models/player';
+import { Ship } from '../GameObjects/Ship';
 
 export class TrainingScene extends Phaser.Scene {
+    private ship!: Ship;
+    private mainEngineKey!: Phaser.Input.Keyboard.Key;
+    private leftAuxEngineKey!: Phaser.Input.Keyboard.Key;
+    private rightAuxEngineKey!: Phaser.Input.Keyboard.Key;
+
     constructor() {
         super({ key: 'TrainingScene' });
     }
@@ -16,9 +23,30 @@ export class TrainingScene extends Phaser.Scene {
         const scaleY = this.cameras.main.height / backgroundImage.height;
         const scale = Math.max(scaleX, scaleY);
         backgroundImage.setScale(scale).setScrollFactor(0);
+
+        // Create ship
+        this.ship = new Ship(this, 200, 600, 'ship', 'TRAINING', '12345678', '🪆', '#FECB00');
+
+        // Create key-binding
+        this.mainEngineKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+        this.leftAuxEngineKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+        this.rightAuxEngineKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     update(): void {
-        // TODO
+        // update actions manualy
+        let rotate = LanderRotation.NONE;
+        if (this.leftAuxEngineKey.isDown && this.rightAuxEngineKey.isUp) {
+            rotate = LanderRotation.COUNTERCLOCKWISE;
+        } else if (this.leftAuxEngineKey.isUp && this.rightAuxEngineKey.isDown) {
+            rotate = LanderRotation.CLOCKWISE;
+        }
+
+        this.ship.actions = {
+            thrust: this.mainEngineKey.isDown,
+            rotate: rotate
+        };
+
+        this.ship.update();
     }
 }
