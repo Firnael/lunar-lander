5d8eabc10705c2c5ea75be045c33707777e3870e
Audren <audren.burlot@gmail.com>
2023-06-09 16:21:11 +0200

    🔊 client - improved telemetry logs

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,9 +1,13 @@
 import { randomUUID } from "crypto"
+import readline from 'readline';
 import { io, Socket } from "socket.io-client"
 import { LanderData } from "../models/lander"
 import { PlayerActions } from "../models/player"
 
 let socket: Socket
+
+const LOG_EVERY_N_FRAMES = 10;
+let frameCounter = 0;
 
 const service = {
 
@@ -33,7 +37,16 @@ const service = {
 
     handleLander: function (callback: (data: LanderData) => PlayerActions) {
         socket.on("landerData", (payload: LanderData) => {
-            console.log('Your lander data arrived from the moon: ', payload);
+            frameCounter++;
+            if (frameCounter % LOG_EVERY_N_FRAMES === 0) {
+                const blank = '\n'.repeat(process.stdout.rows);
+                console.log(blank);
+                readline.cursorTo(process.stdout, 0, 0);
+                readline.clearScreenDown(process.stdout);
+                console.log('Your lander data arrived from the moon: ', payload);
+                frameCounter = 0;
+            }
+            
             // if simulator crashes and restarts, this may be undefined
             if (payload) {
                 const actions: PlayerActions = callback(payload);
